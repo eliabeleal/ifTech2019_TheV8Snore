d357d7ccc4e1199d3aaea448ae5cc48e35b37080
Eliabe Leal <elyabeleal@yahoo.com.br>
2019-11-29 09:35:31 -0300

    complete backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send(`Bem vindo ao mundo JS, ${req.query.name}`);
-});
+const databaseURI = 'mongodb://mongo:27017/sect2019'
 
-app.listen(8081, '0.0.0.0');
+mongoose.connect(
+    databaseURI,
+    {
+        useNewUrlParser: true,
+        autoIndex: false
+    })
+    .then(result => {
+        console.log('MongoDB Conectado');
+    })
+    .catch(error => {
+        console.error(`[SERVER_ERROR] MongoDB initial connection errors:`, error);
+        // process.exit(1);
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use(routes);
+
+app.listen(3000);
+
