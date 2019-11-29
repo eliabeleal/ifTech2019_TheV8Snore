574d0787838d4f3d0acf00445c290d0dfa022a62
Eliabe Leal <elyabeleal@yahoo.com.br>
2019-11-29 10:34:17 -0300

    initial course

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-const routes = require('./routes');
 const app = express();
 
-const databaseURI = 'mongodb://mongo:27017/sect2019'
-
-mongoose.connect(
-    databaseURI,
-    {
-        useNewUrlParser: true,
-        autoIndex: false
-    })
-    .then(result => {
-        console.log('MongoDB Conectado');
-    })
-    .catch(error => {
-        console.error(`[SERVER_ERROR] MongoDB initial connection errors:`, error);
-        // process.exit(1);
-    })
-
-app.use(cors())
-app.use(express.json())
-app.use(routes);
+app.get('/', (req, res) => {
+    return res.send(`Bem vindo ao mundo JS ${req.query.name}`);
+});
 
 app.listen(8080);
 
